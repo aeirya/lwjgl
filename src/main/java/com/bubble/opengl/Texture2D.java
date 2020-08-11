e55a4f78804e9f097dae3addc3b6d96b3dca6982
Sadra Saderi <sadrassx@gmail.com>
2020-08-12 01:08:44 +0430

    Updated Texture2D and VertexBuffer
    + Added destroi() to VertexBuffer
    + Added line 33 and 34 (idk what to say)

--- src/main/java/com/bubble/opengl/Texture2D.java
+++ src/main/java/com/bubble/opengl/Texture2D.java
@@ -30,6 +30,8 @@ public class Texture2D extends Texture {
     @Override
     public void upload() {
         bind();
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
