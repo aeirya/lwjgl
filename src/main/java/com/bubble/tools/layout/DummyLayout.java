2551367dd4bb6a0c53b7ab8a232936ef58cc186d
aeirya <aeirya@gmail.com>
2020-08-13 04:09:38 +0430

    adding "layout" package. used for loading ui layouts

---
+++ src/main/java/com/bubble/tools/layout/DummyLayout.java
@@ -0,0 +1,41 @@
+package com.bubble.tools.layout;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.bubble.util.file.FileLoader;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class DummyLayout extends Layout {
+    public DummyLayout() {
+        elements = Arrays.asList(new Element("id", "panel", new Dimension(10, 20), new Position(1, 3),
+                Arrays.asList(new DummyElement(), new DummyElement()), "text"));
+    }
+
+    public class DummyElement extends Element {
+        public DummyElement() {
+            super("id2", "button", new Dimension(2, 3), new Position(3, 2), null, "click");
+        }
+    }
+
+    public static void main(String[] args) {
+        write();
+    }
+    
+    public static void write() {
+        try (FileWriter fw = new FileWriter("file.txt")) {
+            fw.write(
+                new GsonBuilder().setPrettyPrinting().create().toJson(new DummyLayout())
+            );
+        } catch(IOException e) {
+            //
+        }
+    }
+
+    public static void load() {
+        Layout layout =  new Gson().fromJson(new FileLoader("file.txt").load(), Layout.class);
+        System.out.println(layout.toString());
+    }
+}
---
+++ src/main/java/com/bubble/tools/layout/Layout.java
@@ -0,0 +1,94 @@
+package com.bubble.tools.layout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.gson.GsonBuilder;
+
+public class Layout {
+    
+    protected List<Element> elements;
+
+    public Layout() {
+        elements = new ArrayList<>();
+    }
+
+    public List<Element> getElementsByType(String type) {
+        return elements.stream().filter(e -> e.type.equals(type)).collect(Collectors.toList());
+    }
+
+    public Element getElementById(String id) {
+        return elements.stream().filter(e -> e.id.equals(id)).findFirst().orElse(null);
+    }
+
+    class Element {
+        private String id;
+        private String type;
+        private Position position;
+        private Dimension size;
+        private String text;
+        private String font;
+        private Color color;
+        private String texture;
+        private boolean isDisabled;
+        private MouseEvent events;
+
+        private List<Element> children;
+
+        public Element(String id, String type, Dimension size, Position position, List<Element> children, String text) {
+            this.id = id;
+            this.type = type;
+            this.size = size;
+            this.position = position;
+            this.children = children;
+            this.text = text;
+        }
+    }
+
+    class Dimension {
+        private int width;
+        private int height;
+
+        public Dimension(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    class Position {
+        private int x;
+        private int y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Color {
+        private int r;
+        private int g;
+        private int b;
+        private int a;
+
+        public Color(int r, int g, int b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+
+    class MouseEvent {
+        private String onMouseEnter;
+        private String onMouseExit;
+        private String onMouseClick;
+        private String onMouseUnclick;
+        private String onMouseDrag;
+    }
+
+    @Override
+    public String toString() {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(this);
+    }
+}
