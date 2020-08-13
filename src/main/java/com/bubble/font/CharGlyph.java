d81c5b22deed4eacf6309edea03958691a86bcb8
Sadra Saderi <sadrassx@gmail.com>
2020-08-14 02:00:25 +0430

    Edited CharGlyph

--- src/main/java/com/bubble/font/CharGlyph.java
+++ src/main/java/com/bubble/font/CharGlyph.java
@@ -1,6 +1,9 @@
 package com.bubble.font;
 
-import com.ahraman.unbounded.client.renderer.vertex.VertexBuilder;
+import com.bubble.opengl.Vec2D;
+import com.bubble.opengl.Vertex;
+import com.bubble.opengl.VertexBufferBuilder;
+import com.bubble.std.Color;
 
 public class CharGlyph
 {
@@ -21,19 +24,31 @@ public class CharGlyph
 		this.textureEndV = v1;
 	}
 
-	public void draw(VertexBuilder builder, float x, float y, float scale, Font font)
+	public void draw(VertexBufferBuilder builder, float x, float y, float scale, Font font, Color color)
 	{
 		float minX = x + this.info.getMinX() * scale;
 		float minY = y + this.info.getMinY() * scale;
 		float maxX = x + this.info.getMaxX() * scale;
 		float maxY = y + this.info.getMaxY() * scale;
 		builder.begin();
-		builder.vertex(minX, minY, 0.0F, this.textureStartU, this.textureEndU);
-		builder.vertex(minX, maxY, 0.0F, this.textureStartU, this.textureStartV);
-		builder.vertex(maxX, minY, 0.0F, this.textureEndU, this.textureEndV);
-		builder.vertex(maxX, maxY, 0.0F, this.textureEndU, this.textureStartU);
-		builder.triangle(1, 0, 2);
-		builder.triangle(1, 2, 3);
+		// builder.vertex(minX, minY, 0.0F, this.textureStartU, this.textureEndU);
+		// builder.vertex(minX, maxY, 0.0F, this.textureStartU, this.textureStartV);
+		// builder.vertex(maxX, minY, 0.0F, this.textureEndU, this.textureEndV);
+		// builder.vertex(maxX, maxY, 0.0F, this.textureEndU, this.textureStartU);
+		builder.addVertex(new Vertex(minX, minY, 0.0f));
+		builder.addVertex(new Vec2D(this.textureStartU, this.textureEndU));
+        builder.addVertex(new Vertex(color.r, color.g, color.b));
+		builder.addVertex(new Vertex(maxX, maxY, 0.0f));
+		builder.addVertex(new Vec2D(this.textureStartU, this.textureStartV));
+        builder.addVertex(new Vertex(color.r, color.g, color.b));
+		builder.addVertex(new Vertex(minX, minY, 0.0f));
+		builder.addVertex(new Vec2D(this.textureEndU, this.textureEndV));
+        builder.addVertex(new Vertex(color.r, color.g, color.b));
+		builder.addVertex(new Vertex(maxX, maxY, 0.0f));
+		builder.addVertex(new Vec2D(this.textureEndU, this.textureStartU));
+        builder.addVertex(new Vertex(color.r, color.g, color.b));
+		builder.addTriangle(1, 0, 2);
+		builder.addTriangle(1, 2, 3);
 		builder.end();
 	}
 
